1c3a6c63bf4b6ac511c4a8bb8b915a20a297bfe5
Marina Muñoz <marinamuca01@gmail.com>
2023-06-28 21:37:55 +0200

    Illustration model

--- src/Pages/GltfScene.tsx
+++ src/Pages/GltfScene.tsx
@@ -3,6 +3,8 @@ import { Canvas } from '@react-three/fiber';
 import {Box, Picker} from "../components/Box";
 import { OrbitControls, Environment } from '@react-three/drei';
 import { Room } from '../components/Room';
+import { Wall } from '../components/Wall';
+import { Illustration } from '../components/Illustration';
 
 const GltfScene = () => {
     return (
@@ -11,8 +13,8 @@ const GltfScene = () => {
             <Canvas>
                 <ambientLight intensity={0.5}/>
                 <Suspense fallback={null}>
-                    {/* <Wall rotation={[0,-Math.PI/2,0]}/> */}
-                    <Room rows={9} cols={3}></Room>
+                    <Wall/>
+                    <Illustration meshProps={ {position: [-0.9999,1.3,0], rotation: [0,Math.PI/2,0]} } imgUrl='textures/1.jpg'></Illustration>
                     <Environment files="textures/hdr.hdr"/>
                 </Suspense>
                 <OrbitControls/>
